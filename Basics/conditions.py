ced2fe988c4977c1c092eb4ba3ddb8c6ab6e703b
Wilfred Castelino <wc335@njit.edu>
2019-02-24 14:01:23 -0500

    Exercises on loops added

--- Basics/conditions.py
+++ Basics/conditions.py
@@ -63,12 +63,63 @@ while True:
             started = False
             print('Car Stopped')
     elif command=='help':
-        print('''
-Start - to start the car
-Stop - to stop the car
-Quit - to exit
-''')
+        print('Start - to start the car Stop - to stop the car Quit - to exit')
+
     elif command=='quit':
         break
     else:
         print("Sorry i don't understand that")
+
+#------------FOR LOOP--------------------------------------------------------------
+
+prices = [10,20,30]
+
+total = 0
+for items in prices:
+    total +=items
+print(f' The total of the items is: {total}')
+
+#-----------EXERCISE: FOR LOOPS-----------------------------------------------
+
+numbers = [5,2,5,2,2]
+for x in numbers:
+    print(x * 'X')
+
+#----------EXERCISE:NESTED LOOPS (Print F)----------------------------------------------
+
+numbers = [5,2,5,2,2]
+numbers_l = [1,1,1,1,5] #------Print l
+
+for x_count in numbers_l:
+    output = ''
+    for y in range(x_count):
+        output += 'X'
+    print(output)
+
+#----------EXERCISE: FIND THE LARGEST NO IN A LIST---------------------------------------
+
+numbers = [2,8,19,32,3,5]
+max =0
+for item in numbers:
+    if item > max:
+        max = item
+print(f'The largest number in the list is {max}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
